d5c748382af67fa82fa1e67197724c72e372779f
Micky Chen <mickychen0524@yahoo.com>
2017-11-03 12:03:46 +0800

    update codebase

--- app/src/main/java/lite/storeclerk/admin/playlazlo/com/storeclerklite/App.java
+++ app/src/main/java/lite/storeclerk/admin/playlazlo/com/storeclerklite/App.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.support.multidex.MultiDex;
 import android.support.multidex.MultiDexApplication;
 
-import project.labs.avviotech.com.chatsdk.nearby.NearByUtil;
-
 /**
  * Created by mymac on 4/3/17.
  */
@@ -15,16 +13,5 @@ public class App extends MultiDexApplication {
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
         MultiDex.install(this);
-    }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-    }
-
-    @Override
-    public void onTerminate() {
-        super.onTerminate();
-
     }
 }
